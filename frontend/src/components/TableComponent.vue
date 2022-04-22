af8a5e4c41b0e4a59cc4c7b6e0052ff96d9fbb22
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-04-22 09:32:40 -0300

    home layout

---
+++ frontend/src/components/TableComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <table class="border-2 border-black h-16 w-9/12">
+            <thead class="border-2 border-black">
+                <tr>
+                    <th>Nome</th>
+                    <th>Inicio</th>
+                    <th>Termino</th>
+                    <th>Valor</th>
+                    <th>Risco</th>
+                    <th>Participantes</th>
+                    <th>Possivel retorno</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="pl-12">
+                    <td>Teste</td>
+                    <td>12/03/94</td>
+                    <td>05/03/2001</td>
+                    <td>2423423</td>
+                    <td>Alto</td>
+                    <td>Participants</td>
+                    <td>Possievel retorno</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TableComponent',
+    data:() => ({
+
+    }),
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="home">
-    <div>
-      <h1>Hello World</h1>
+  <div class="md:flex flex-row justify-start bg-gradient-to-b from-green-300 to-green-800">
+    <div class="md:flex flex-col justify-start items-center bg-black h-screen w-96 pt-4">
+      <img class="h-16 w-42" src="../assets/img/logo.png" alt="">
+      <div>
+        <nav>
+          <button class="text-green-800 font-bold border-2 border-white h-12 w-44 mt-4 rounded-md bg-white">
+            <router-link to="/cadastro">Cadastrar Projeto</router-link> 
+          </button>
+        </nav>
+      </div>
+    </div>
+    <div class="md:flex flex-col justify-start items-center pt-8 w-screen "> 
+      <div class="md:flex flex-col pt-12 w-9/12 min-h-full bg-white pl-40 w-11/12 rounded-md">
+        <h1 class="ml-36 text-2xl font-bold">Projetos em Andamento</h1>
+        <TableComponent class="mt-4 h-fit w-fit" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import TableComponent from '../components/TableComponent.vue';
 
 export default {
   name: 'Home',
+  components: {
+    TableComponent
+  },
   data:() => ({
 
   }),
