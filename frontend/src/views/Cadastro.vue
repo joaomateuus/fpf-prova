b36951367ab62d67c19aa68cc0e2c1f465a0f024
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-04-21 21:10:33 -0300

    first commit

---
+++ frontend/src/views/Cadastro.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Cadastro',
+    data:() => ({
+
+    }),
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data:() => ({
+
+  }),
+ 
+}
+</script>
